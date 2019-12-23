6d0f1341474d327cf51f80711c285030a8a63816
Michael Diaz <mike.diaz006@gmail.com>
2019-12-23 23:38:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -65,7 +65,7 @@ def run(songs)
       puts "Please enter a command:"
       input = gets.strip
     elsif input == "play"
-      play(songs)
+      play
       puts "Please enter a command:"
       input = gets.strip
     elsif input == "exit"
