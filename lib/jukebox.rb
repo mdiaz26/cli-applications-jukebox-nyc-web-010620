3806f4bd060bef5e573fc1edff8a06fa0fc0851e
Michael Diaz <mike.diaz006@gmail.com>
2019-12-23 23:25:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,71 @@
-# Add your code here
+require "pry"
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.strip
+ 
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  if (1..9) === input
+    # song_choice = songs[input - 1]
+    puts "Playing"
+    puts "Playing #{song_choice}"
+  # elsif songs.any? {|song| input == song}
+  #   puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  until input == "exit" do
+    input = gets.strip
+    if input == "help"
+      help
+    elsif input == "list"
+      list(songs)
+    elsif input == "play"
+      play
+    else
+      puts "Please enter a command:"
+      input = gets.strip
+    end
+      exit_jukebox
+  end
+end
